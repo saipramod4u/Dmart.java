2dad4b9513e96fbaa72ebf251163a5b30e2ff8b1
sai <sai3195@gmail.com>
2024-10-16 13:42:20 +0000

    remodified signup page

--- helloworld.java
+++ helloworld.java
@@ -10,13 +10,7 @@ public class StandardDeviation {
     public static double calculateSD(double numArray[])
     {
         double sum = 0.0, standardDeviation = 0.0;
-        int length = numArray.length;
-
-        for(double num : numArray) {
-            sum += num;
-        }
-
-        double mean = sum/length;
+        hey hey yo yo yo
 
         for(double num: numArray) {
             standardDeviation += Math.pow(num - mean, 2);
@@ -24,4 +18,4 @@ public class StandardDeviation {
 
         return Math.sqrt(standardDeviation/length);
     }
- hey hey wassup wassup mates
+ hey hey wassup wassup mate			
